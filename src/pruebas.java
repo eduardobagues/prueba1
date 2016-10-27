bece3e7324b9951262746db11f7bee677f459b82
eduardo <eduardobaguespardo@gmail.com>
2016-10-27 18:42:01 +0200

    Primer proyecto con Java

---
+++ src/pruebas.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+public class pruebas {
+
+	public static void main(String[] args) {
+		
+	
+		
+		int n = 0;
+		int i = 1;
+		do {
+		System.out.print(i);
+		i++;
+		} while ( i <= n );
+		
+	
+	
+		
+		
+
+	}
+
+}
